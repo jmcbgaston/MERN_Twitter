d0ee8eca4af9d50d97647a0f3dda56c360edf103
Jose Gaston <jmcbgaston@gmail.com>
2020-10-25 10:50:54 -0700

    Add validations

--- routes/api/users.js
+++ routes/api/users.js
@@ -5,16 +5,25 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const keys = require('../../config/keys');
 const passport = require('passport');
+const validateRegisterInput = require('../../validation/register');
+const validateLoginInput = require('../../validation/login');
 
 router.get("/test", (req, res) => {
     res.json({ msg: "This is the users route" })
 });
 
 router.post("/register", (req, res) => {
-    User.findOne({email: req.body.email})
+
+    const { errors, isValid } = validateRegisterInput(req.body);
+
+    if (!isValid) {
+        return res.status(400).json(errors);
+    }
+
+    User.findOne({ email: req.body.email })
         .then(user => {
             if (user) {
-                return res.status(400).json({email: "User already registered"})
+                return res.status(400).json({ email: "User already registered" })
             } else {
                 const newUser = new User({
                     handle: req.body.handle, 
@@ -36,13 +45,20 @@ router.post("/register", (req, res) => {
 })
 
 router.post('/login', (req, res) => {
+
+    const { errors, isValid } = validateLoginInput(req.body);
+
+    if (!isValid) {
+        return res.status(400).json(errors);
+    }
+
     const email = req.body.email;
     const password = req.body.password;
 
     User.findOne({ email })
         .then(user => {
             if (!user) {
-                return res.status(404).json({email: "This user does not exist"});
+                return res.status(404).json({ email: "This user does not exist" });
             }
 
             bcrypt.compare(password, user.password)
@@ -65,13 +81,13 @@ router.post('/login', (req, res) => {
                             }
                         )
                     } else {
-                        return res.status(400).json({password: "Incorrect password"});
+                        return res.status(400).json({ password: "Incorrect password" });
                     }
                 })
         }) 
 })
 
-router.get('/current', passport.authenticate('jwt', {session: false}), (req, res) => {
+router.get('/current', passport.authenticate('jwt', { session: false }), (req, res) => {
     res.json({
       id: req.user.id,
       handle: req.user.handle,
@@ -79,8 +95,8 @@ router.get('/current', passport.authenticate('jwt', {session: false}), (req, res
     });
   })
 
-router.get('/current', passport.authenticate('jwt', {session: false}), (req, res) => {
-    res.json({msg: 'Success'});
+router.get('/current', passport.authenticate('jwt', { session: false }), (req, res) => {
+    res.json({ msg: 'Success' });
 })
 
 
