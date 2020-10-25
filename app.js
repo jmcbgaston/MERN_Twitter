43bc47d97e75e880fbc795f277b959ca74e12e62
Jose Gaston <jmcbgaston@gmail.com>
2020-10-25 01:01:54 -0700

    Add routes

--- app.js
+++ app.js
@@ -4,17 +4,33 @@ const app = express();
 const db = require('./config/keys').mongoURI;
 const users = require("./routes/api/users");
 const tweets = require("./routes/api/tweets");
+const User = require('./models/User');
 const bodyParser = require('body-parser');
+const passport = require('passport');
 
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
 
+app.use(bodyParser.urlencoded({
+  extended: false
+}))
+
+app.use(bodyParser.json());
+
 app.get("/", (req, res) => {
-  console.log(res);
+  const user = new User({
+    handle: "user", 
+    email: "user@test.com", 
+    password: "password"
+  })
+  user.save()
   res.send("MERN Twitter");
 })
+
+app.use(passport.initialize());
+require('./config/passport')(passport);
 
 app.use("/api/users", users);
 app.use("/api/tweets", tweets);
