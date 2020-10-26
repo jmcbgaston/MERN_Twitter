922e2a2439d60742b4bab2f0c4c91eb06d9a5413
Jose Gaston <jmcbgaston@gmail.com>
2020-10-25 21:54:18 -0700

    Completed MERN Twitter

---
+++ frontend/src/reducers/root_reducer.js
@@ -0,0 +1,21 @@
+// import { combineReducers } from 'redux';
+// import session from './session_reducer';
+
+// const RootReducer = combineReducers({
+//   session
+// });
+
+// export default RootReducer;
+
+import { combineReducers } from 'redux';
+import session from './session_reducer';
+import errors from './errors_reducer';
+import tweets from './tweets_reducer';
+
+const RootReducer = combineReducers({
+  errors,
+  session,
+  tweets
+});
+
+export default RootReducer;
